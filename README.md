2f36091ccb380bf516411737e5937ad605d33da4
keer2345 <keer2345@gmail.com>
2022-01-17 19:42:30 +0800

    Chinese Font

---
+++ README.md
@@ -0,0 +1,2 @@
+## Materials 
+- https://space.bilibili.com/479651586
--- config.h
+++ config.h
@@ -5,7 +5,10 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+/*static const char *fonts[]          = { "monospace:size=10" };*/
+static const char *fonts[]          = { "Hack:size=10",
+	"WenQuanYi Micro Hei:size=10:type=Regular:antialias=true:autohint=true",
+	"Symbols Nerd Font:pixelsize=14:type=2048-em:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
